c549c6991c5b3fd3a679ba002ffc8276474ef9b5
Bianca Cristina  da Silva <bianca.silva@vtex.com>
2024-09-12 12:17:28 -0300

    feat(SFS-1439): add sort_by

--- custom_cel/lists.go
+++ custom_cel/lists.go
@@ -2,9 +2,14 @@ package custom_cel
 
 import (
 	"github.com/google/cel-go/cel"
+	"github.com/google/cel-go/common"
+	"github.com/google/cel-go/common/ast"
+	"github.com/google/cel-go/common/operators"
 	"github.com/google/cel-go/common/types"
 	"github.com/google/cel-go/common/types/ref"
 	"github.com/google/cel-go/common/types/traits"
+	"github.com/google/cel-go/ext"
+	"github.com/google/cel-go/parser"
 	"k8s.io/apiserver/pkg/cel/library"
 	"sort"
 	"strings"
@@ -16,17 +21,20 @@ const (
 )
 
 // Lists returns a cel.EnvOption to configure extended functions Lists manipulation.
-// # Sort
+// # SortBy
 //
-// Returns a new sorted list by the order defined (ascending or descending).
+// Returns a new sorted list by the field and order defined (ascending or descending).
 // It supports all types that implements the base traits.Comparer interface.
 //
-// sort(<list>, "order") -> <list>
+// <list>.sort_by(obj, obj.field) -> <list>
 //
 // Examples:
 //
-// sort(["c", "b", "a"], "asc") // return ["a", "b", "c"]
-// sort([2, 1, 3], "desc")        // return [3, 2, 1]
+// [3,1,2].sort_by(i, i) // returns [1,2,3]
+//
+// [3,1,2].sort_by(i, i, "desc") // returns [3,2,1]
+//
+// [{Name: "c", Age: 10}, {Name: "a", Age: 30}, {Name: "b", Age: 1}].sort_by(obj, obj.age) // returns [{Name: "b", Age: 1}, {Name: "c", Age: 10}, {Name: "a", Age: 30}]
 func Lists() cel.EnvOption {
 	return cel.Lib(listsLib{})
 }
@@ -36,8 +44,22 @@ type listsLib struct{}
 // CompileOptions implements the Library interface method defining the basic compile configuration
 func (u listsLib) CompileOptions() []cel.EnvOption {
 	dynListType := cel.ListType(cel.DynType)
+	sortByMacro := parser.NewReceiverMacro("sort_by", 2, makeSortBy)
+	sortByMacroWithOrder := parser.NewReceiverMacro("sort_by", 3, makeSortBy)
 	return []cel.EnvOption{
+		ext.Strings(),
 		library.Lists(),
+		cel.Macros(sortByMacro),
+		cel.Macros(sortByMacroWithOrder),
+		cel.Function(
+			"pair",
+			cel.Overload(
+				"make_pair",
+				[]*cel.Type{cel.DynType, cel.DynType},
+				cel.DynType,
+				cel.BinaryBinding(makePair),
+			),
+		),
 		cel.Function(
 			"sort",
 			cel.Overload(
@@ -55,6 +77,26 @@ func (u listsLib) ProgramOptions() []cel.ProgramOption {
 	return []cel.ProgramOption{}
 }
 
+type pair struct {
+	order ref.Val
+	value ref.Val
+}
+
+var (
+	orderKey = types.DefaultTypeAdapter.NativeToValue("order")
+	valueKey = types.DefaultTypeAdapter.NativeToValue("value")
+)
+
+func makePair(order ref.Val, value ref.Val) ref.Val {
+	if _, ok := order.(traits.Comparer); !ok {
+		return types.ValOrErr(order, "unable to build ordered pair with value %v", order.Value())
+	}
+	return types.NewStringInterfaceMap(types.DefaultTypeAdapter, map[string]any{
+		"order": order.Value(),
+		"value": value.Value(),
+	})
+}
+
 func sortByOrder(itemsVal ref.Val, orderVal ref.Val) ref.Val {
 	items, ok := itemsVal.(traits.Lister)
 	if !ok {
@@ -66,15 +108,24 @@ func sortByOrder(itemsVal ref.Val, orderVal ref.Val) ref.Val {
 		return types.ValOrErr(orderVal, "unable to convert to ref.Val string")
 	}
 
-	itemsSlice := make([]ref.Val, 0)
+	pairs := make([]pair, 0, items.Size().Value().(int64))
+	index := 0
 	for it := items.Iterator(); it.HasNext().(types.Bool); {
-		curr := it.Next()
-		itemsSlice = append(itemsSlice, curr)
+		curr, ok := it.Next().(traits.Mapper)
+		if !ok {
+			return types.NewErr("unable to convert elem %d to traits.Mapper", index)
+		}
+
+		pairs = append(pairs, pair{
+			order: curr.Get(orderKey),
+			value: curr.Get(valueKey),
+		})
+		index++
 	}
 
 	ascSort := func(i, j int) bool {
-		cmp := itemsSlice[i].(traits.Comparer)
-		switch cmp.Compare(itemsSlice[j]) {
+		cmp := pairs[i].order.(traits.Comparer)
+		switch cmp.Compare(pairs[j].order) {
 		case types.IntNegOne:
 			return true
 		case types.IntOne:
@@ -85,8 +136,8 @@ func sortByOrder(itemsVal ref.Val, orderVal ref.Val) ref.Val {
 	}
 
 	descSort := func(i, j int) bool {
-		cmp := itemsSlice[i].(traits.Comparer)
-		switch cmp.Compare(itemsSlice[j]) {
+		cmp := pairs[i].order.(traits.Comparer)
+		switch cmp.Compare(pairs[j].order) {
 		case types.IntNegOne:
 			return false
 		case types.IntOne:
@@ -98,12 +149,72 @@ func sortByOrder(itemsVal ref.Val, orderVal ref.Val) ref.Val {
 
 	switch strings.ToLower(order) {
 	case AscendingOrder:
-		sort.Slice(itemsSlice, ascSort)
+		sort.Slice(pairs, ascSort)
 	case DescendingOrder:
-		sort.Slice(itemsSlice, descSort)
+		sort.Slice(pairs, descSort)
 	default:
 		return types.NewErr("unknown order: %s", order)
 	}
 
-	return types.NewDynamicList(types.DefaultTypeAdapter, itemsSlice)
+	var ordered []interface{}
+	for _, v := range pairs {
+		ordered = append(ordered, v.value.Value())
+	}
+
+	return types.NewDynamicList(types.DefaultTypeAdapter, ordered)
+}
+
+func extractIdent(e ast.Expr) (string, bool) {
+	if e.Kind() == ast.IdentKind {
+		return e.AsIdent(), true
+	}
+	return "", false
+}
+
+func extractOrder(args []ast.Expr) ref.Val {
+	if len(args) == 3 {
+		return args[2].AsLiteral()
+	}
+	return types.String("asc")
+}
+
+func makeSortBy(eh parser.ExprHelper, target ast.Expr, args []ast.Expr) (ast.Expr, *common.Error) {
+	v, found := extractIdent(args[0])
+	if !found {
+		return nil, eh.NewError(args[0].ID(), "argument is not an identifier")
+	}
+
+	var fn = args[1]
+
+	init := eh.NewList()
+	condition := eh.NewLiteral(types.True)
+
+	step := eh.NewCall(operators.Add, eh.NewAccuIdent(), eh.NewList(
+		eh.NewCall("pair", fn, args[0]),
+	))
+
+	order := extractOrder(args)
+
+	/*
+	   This comprehension is expanded to:
+	   __result__ = [] # init expr
+	   for $v in $target:
+	       __result__ += [pair(fn(v), v)] # step expr
+	   return sort(__result__, "asc") # result expr
+	*/
+	mapped := eh.NewComprehension(
+		target,
+		v,
+		parser.AccumulatorName,
+		init,
+		condition,
+		step,
+		eh.NewCall(
+			"sort",
+			eh.NewAccuIdent(),
+			eh.NewLiteral(types.DefaultTypeAdapter.NativeToValue(order)),
+		),
+	)
+
+	return mapped, nil
 }
--- custom_cel/lists_test.go
+++ custom_cel/lists_test.go
@@ -13,155 +13,31 @@ func Test_sortByOrder(t *testing.T) {
 	varName := "objects"
 
 	now := time.Now()
-	first := now.Add(-(time.Duration(24) * time.Hour * 3))
-	second := now.Add(-(time.Duration(24) * time.Hour * 2))
-	third := now.Add(-(time.Duration(24) * time.Hour * 1))
+	first := now.Add(-(time.Duration(24) * time.Hour * 3)).Format(time.RFC3339Nano)
+	second := now.Add(-(time.Duration(24) * time.Hour * 2)).Format(time.RFC3339Nano)
+	third := now.Add(-(time.Duration(24) * time.Hour * 1)).Format(time.RFC3339Nano)
 
 	testCases := map[string]struct {
 		condition string
-		list      any
+		ul        map[string]interface{}
 		wantUlDyn ref.Val
 	}{
-		"sort timestamp list in descending order": {
-			condition: `sort(objects, "desc")`,
-			list:      []time.Time{second, first, third},
-			wantUlDyn: types.NewDynamicList(
-				types.DefaultTypeAdapter,
-				[]types.Timestamp{
-					{Time: third},
-					{Time: second},
-					{Time: first},
-				}),
+		"sort unstructured in ascending order by default": {
+			condition: `objects.items.sort_by(v, v.metadata.creationTimestamp)`,
+			ul:        generateUnorderedUl(t, first, second, third),
+			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, generateOrderedSlice(t, first, second, third)),
 		},
 
-		"sort timestamp list in ascending order": {
-			condition: `sort(objects, "asc")`,
-			list:      []time.Time{second, first, third},
-			wantUlDyn: types.NewDynamicList(
-				types.DefaultTypeAdapter,
-				[]types.Timestamp{
-					{Time: first},
-					{Time: second},
-					{Time: third},
-				}),
+		"sort unstructured in descending order": {
+			condition: `objects.items.sort_by(v, v.metadata.creationTimestamp, "desc")`,
+			ul:        generateUnorderedUl(t, first, second, third),
+			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, generateOrderedSlice(t, third, second, first)),
 		},
 
-		"sort duration list in descending order": {
-			condition: `sort(objects, "desc")`,
-			list: []time.Duration{
-				time.Duration(second.Unix()),
-				time.Duration(first.Unix()),
-				time.Duration(third.Unix()),
-			},
-			wantUlDyn: types.NewDynamicList(
-				types.DefaultTypeAdapter,
-				[]types.Duration{
-					{Duration: time.Duration(third.Unix())},
-					{Duration: time.Duration(second.Unix())},
-					{Duration: time.Duration(first.Unix())},
-				}),
-		},
-
-		"sort duration list in ascending order": {
-			condition: `sort(objects, "asc")`,
-			list: []time.Duration{
-				time.Duration(second.Unix()),
-				time.Duration(first.Unix()),
-				time.Duration(third.Unix()),
-			},
-			wantUlDyn: types.NewDynamicList(
-				types.DefaultTypeAdapter,
-				[]types.Duration{
-					{Duration: time.Duration(first.Unix())},
-					{Duration: time.Duration(second.Unix())},
-					{Duration: time.Duration(third.Unix())},
-				}),
-		},
-
-		"sort int list in descending order": {
-			condition: `sort([2,1,3], "desc")`,
-			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, []types.Int{3, 2, 1}),
-		},
-
-		"sort int list in ascending order": {
-			condition: `sort([2,1,3], "asc")`,
-			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, []types.Int{1, 2, 3}),
-		},
-
-		"sort uint list in descending order": {
-			condition: `sort([uint(2), uint(1), uint(3)], "desc")`,
-			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, []types.Uint{3, 2, 1}),
-		},
-
-		"sort uint list in ascending order": {
-			condition: `sort([uint(2), uint(1), uint(3)], "asc")`,
-			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, []types.Uint{1, 2, 3}),
-		},
-
-		"sort double list in descending order": {
-			condition: `sort([double(2), double(1), double(3)], "desc")`,
-			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, []types.Double{3, 2, 1}),
-		},
-
-		"sort double list in ascending order": {
-			condition: `sort([double(2), double(1), double(3)], "asc")`,
-			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, []types.Double{1, 2, 3}),
-		},
-
-		"sort bytes list in descending order": {
-			condition: `sort([bytes("c"), bytes("a"), bytes("b")], "desc")`,
-			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, []types.Bytes{[]byte("c"), []byte("b"), []byte("a")}),
-		},
-
-		"sort bytes list in ascending order": {
-			condition: `sort([bytes("c"), bytes("a"), bytes("b")], "asc")`,
-			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, []types.Bytes{[]byte("a"), []byte("b"), []byte("c")}),
-		},
-
-		"sort boolean list in descending order": {
-			condition: `sort([true, false, true], "desc")`,
-			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, []types.Bool{true, true, false}),
-		},
-
-		"sort boolean list in ascending order": {
-			condition: `sort([true, false, true], "asc")`,
-			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, []types.Bool{false, true, true}),
-		},
-
-		"sort string list in descending order": {
-			condition: `sort(["c", "a", "b"], "desc")`,
-			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, []types.String{"c", "b", "a"}),
-		},
-
-		"sort string list in ascending order": {
-			condition: `sort(["c", "a", "b"], "asc")`,
-			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, []types.String{"a", "b", "c"}),
-		},
-
-		"sort unstructured list in descending order by timestamp": {
-			condition: `sort(objects.items.map(item, timestamp(item.metadata.creationTimestamp)), "desc")`,
-			list:      generateUnorderedUl(t, first.Format(time.RFC3339Nano), second.Format(time.RFC3339Nano), third.Format(time.RFC3339Nano)),
-			wantUlDyn: types.NewDynamicList(
-				types.DefaultTypeAdapter,
-				[]types.Timestamp{
-					{Time: third},
-					{Time: second},
-					{Time: first},
-				},
-			),
-		},
-
-		"sort unstructured list in ascending order by timestamp": {
-			condition: `sort(objects.items.map(item, timestamp(item.metadata.creationTimestamp)), "asc")`,
-			list:      generateUnorderedUl(t, first.Format(time.RFC3339Nano), second.Format(time.RFC3339Nano), third.Format(time.RFC3339Nano)),
-			wantUlDyn: types.NewDynamicList(
-				types.DefaultTypeAdapter,
-				[]types.Timestamp{
-					{Time: first},
-					{Time: second},
-					{Time: third},
-				},
-			),
+		"sort unstructured in ascending order": {
+			condition: `objects.items.sort_by(v, v.metadata.creationTimestamp, "asc")`,
+			ul:        generateUnorderedUl(t, first, second, third),
+			wantUlDyn: types.NewDynamicList(types.DefaultTypeAdapter, generateOrderedSlice(t, first, second, third)),
 		},
 	}
 
@@ -170,7 +46,7 @@ func Test_sortByOrder(t *testing.T) {
 			prg := setupProgram(t, varName, tc.condition)
 
 			gotUlDyn, _, gotErr := prg.Eval(map[string]interface{}{
-				varName: tc.list,
+				varName: tc.ul,
 			})
 
 			if gotErr != nil {
@@ -239,4 +115,29 @@ func generateUnorderedUl(t *testing.T, first, second, third string) map[string]i
 	}
 
 	return ul.UnstructuredContent()
+}
+
+func generateOrderedSlice(t *testing.T, first, second, third string) []map[string]interface{} {
+	t.Helper()
+	orderedItems := make([]map[string]interface{}, 0)
+	orderedItems = append(orderedItems,
+		map[string]interface{}{
+			"metadata": map[string]interface{}{
+				"creationTimestamp": first,
+			},
+		},
+
+		map[string]interface{}{
+			"metadata": map[string]interface{}{
+				"creationTimestamp": second,
+			},
+		},
+
+		map[string]interface{}{
+			"metadata": map[string]interface{}{
+				"creationTimestamp": third,
+			},
+		},
+	)
+	return orderedItems
 }
